56a07c5c0e43272cfef5fc7383533f78da390f23
gwnlng <108258271+gwnlng@users.noreply.github.com>
2022-08-05 13:34:06 +0800

    workflow improvements with cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
+output "aws_broker_dns_name" {
+  description = "AWS generated SnykBroker Client DNS name"
+  value       = module.snykbroker_nlb.lb_dns_name
+}
+
 output "broker_client_url" {
   description = "SnykBroker Client URL"
   value       = try(lookup(local.broker_env_vars, "BROKER_CLIENT_URL", ""), "")
-  sensitive   = false
+  sensitive   = true
 }
